596d24ed8daec3a19f873e03d93fcb96d1f856a7
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-08-02 21:06:59 +0530

    Update Rangedetect.c

--- Rangedetect.c
+++ Rangedetect.c
@@ -4,21 +4,21 @@
 int DetectRange(int * readings,int size,rangeInfo *Data_struct)
 {
     int totalRange=0;
-    Data_struct[totalRange].rangeStart=readings[0];
-    Data_struct[totalRange].readingsInRange=1;
+    Data_struct[totalRange].RangeStart=readings[0];
+    Data_struct[totalRange].ReadingsInRange=1;
     for(int Readcount=0;Readcount<size-1;Readcount++)
     {
        if((readings[Readcount]+1==readings[Readcount+1])||(readings[Readcount]==readings[Readcount+1]))
        {
 
-       Data_struct[totalRange].rangeEnd=readings[Readcount+1];
-       Data_struct[totalRange].readingsInRange++;
+       Data_struct[totalRange].RangeEnd=readings[Readcount+1];
+       Data_struct[totalRange].ReadingsInRange++;
        }
        else
        {
        totalRange++;
-       Data_struct[totalRange].rangeStart=readings[Readcount+1];
-       Data_struct[totalRange].readingsInRange++;
+       Data_struct[totalRange].RangeStart=readings[Readcount+1];
+       Data_struct[totalRange].ReadingsInRange++;
        }
     }
 	  return(totalRange+1);
@@ -29,13 +29,13 @@ void PrintRangeInfo(rangeInfo *Data_struct,int totalRange)
     printf("\nRange         Count\n");
     for(int Rangecount=0;Rangecount<totalRange;Rangecount++)
     {
-         if(Data_struct[Rangecount].readingsInRange != 1)
+         if(Data_struct[Rangecount].ReadingsInRange != 1)
          {
-           printf("%d - %d , %d\n",Data_struct[Rangecount].rangeStart, Data_struct[Rangecount].rangeEnd,Data_struct[Rangecount].readingsInRange);
+           printf("%d - %d , %d\n",Data_struct[Rangecount].RangeStart, Data_struct[Rangecount].RangeEnd,Data_struct[Rangecount].ReadingsInRange);
          }
          else
          {
-            printf("%d , %d\n",Data_struct[Rangecount].rangeStart,Data_struct[Rangecount].readingsInRange); 
+            printf("%d , %d\n",Data_struct[Rangecount].RangeStart,Data_struct[Rangecount].readingsInRange); 
          }
     }
 
