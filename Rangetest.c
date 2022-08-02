2bd887102e212d18ebcc435a7c8536c7e7438398
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-08-02 21:31:08 +0530

    Update Rangetest.c

--- Rangetest.c
+++ Rangetest.c
@@ -4,13 +4,13 @@
 
 void TEST_CASE_01()
 {
-    int Data[2]={4,5};
+    int Data[]={4,1,1};
     int Totalreadings=sizeof(Data)/sizeof(Data[0]);
     assert(DetectAndDisplayRange(Data,Totalreadings) == 1);
 }
 void TEST_CASE_02()
 {
-    int Data[5]={4,5,5,8,10};
+    int Data[]={4,5,5,6,8,10};
     int Totalreadings=sizeof(Data)/sizeof(Data[0]);
     assert(DetectAndDisplayRange(Data,Totalreadings) == 3);
 }
